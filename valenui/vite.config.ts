fabec28d7d2cbd1f816f7349c1551c74b5f15be4
SimuHireAI <shreyassalian0425@gmail.com>
2026-02-26 01:03:25 +0530

    Some Changes

--- valenui/vite.config.ts
+++ valenui/vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    minify: 'esbuild',
   },
 })
